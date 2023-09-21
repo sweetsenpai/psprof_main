3546b70dd1f22901bf75dcb48921968bdd7f291e
Sweet_Sempai <chcolatemilk00@gmail.com>
2023-09-21 15:57:20 +0300

    display all users in database

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,13 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 from config import app, db
 from DB.db_builder import Users
 
 
 
-
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    users = db.session().query(Users).all()
+    return  render_template('index.html',posts=users)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
