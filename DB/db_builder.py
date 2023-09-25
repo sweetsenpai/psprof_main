529ae39f4e8ddf9d2a44db1b5f4ca238aa032087
Sweet_Sempai <chcolatemilk00@gmail.com>
2023-09-25 16:53:45 +0300

    rework relationship in DB

--- DB/db_builder.py
+++ DB/db_builder.py
@@ -1,15 +1,16 @@
-from flask_app.config import db, app, SQLAlchemy
+from flask_app.config import db, app
+from sqlalchemy.orm import sessionmaker, relationship
+
 
 class Categories(db.Model):
     
     __tablename__ = 'categories'
     category_id = db.Column(name='category_id', type_=db.Integer, primary_key=True)
     category_title = db.Column(name='category_title', type_=db.String)
-
+    subcategories = relationship('Subcategories', backref='categories', cascade='save-update, merge, delete')
 
     def __init__(self, category_title):
         self.category_title = category_title
-
 
     def __repr__(self):
         return f'{self.category_id}, {self.category_title}'
@@ -18,16 +19,15 @@ class Categories(db.Model):
 class Subcategories(db.Model):
     __tablename__ = 'subcategories'
     subcategories_id = db.Column(name='subcategories_id', type_=db.Integer, primary_key=True)
-    subcategories_categories = db.Column(db.Integer, db.ForeignKey('categories.category_id', ondelete="CASCADE"))
+    subcategories_categories = db.Column(db.Integer, db.ForeignKey('categories.category_id'))
     subcategories_titel = db.Column(name='subcategories_titel', type_=db.String)
     subcategories_url = db.Column(name='subcategories_url', type_=db.String, default='')
-    
-    
+    channels = relationship('Channels', backref='subcategories', cascade='save-update, merge, delete')
+
     def __init__(self, subcategories_categories, subcategories_titel, subcategories_url):
         self.subcategories_categories = subcategories_categories
         self.subcategories_titel = subcategories_titel
         self.subcategories_url = subcategories_url
-        
         
     def __repr__(self):
         return f'{self.subcategories_id}, {self.subcategories_categories}, {self.subcategories_titel}, {self.subcategories_url}, {self.display_order}'
@@ -36,17 +36,15 @@ class Subcategories(db.Model):
 class Channels(db.Model):
     __tablename__ = 'channels'
     channel_id = db.Column(name='channel_id', type_=db.Integer, primary_key=True)
-    subcategories_channel = db.Column(db.Integer, db.ForeignKey('subcategories.subcategories_id', ondelete="CASCADE"))
+    subcategories_channel = db.Column(db.Integer, db.ForeignKey('subcategories.subcategories_id'))
     channel_titel = db.Column(name='channel_titel', type_=db.String)
     channel_url = db.Column(name='channel_url', type_=db.String)
-    
     
     def __init__(self, subcategories_channel, channel_titel, channel_url):
         self.subcategories_channel = subcategories_channel
         self.channel_titel = channel_titel
         self.channel_url = channel_url
         self.display_order = self.channel_id
-    
     
     def __repr__(self):
         return f'{self.channel_id}, {self.subcategories_channel}, {self.channel_titel}, {self.channel_url}'
@@ -61,13 +59,11 @@ class Users(db.Model):
     _id = db.Column(name='_id', type_=db.Integer, primary_key=True,)
     user_id = db.Column(name='user_id', type_=db.Integer, index=True)
     user_name = db.Column(name='user_name', type_=db.String)
-    
-    
+
     def __init__(self, user_id, user_name):
         self.user_id = user_id
         self.user_name = user_name
-     
-        
+
     def __repr__(self):
         return f'{self._id}, {self.user_id}, {self.user_name}'
 
